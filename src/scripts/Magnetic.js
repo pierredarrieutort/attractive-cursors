150dda939f6ac5fb5bed5212d3106cfd6c1434c6
pierredarrieutort <p.darrieutort@outlook.fr>
2021-12-15 12:02:51 +0100

    add Magnetic button

---
+++ src/scripts/Magnetic.js
@@ -0,0 +1,45 @@
+import gsap from 'gsap'
+
+export default class Magnetic {
+  constructor (el, options = { y: .2, x: .2, s: .2, rs: .7 }) {
+    this.el = el
+
+    this.options = options
+
+    this.y = 0
+    this.x = 0
+    this.width = 0
+    this.height = 0
+
+    this.bind()
+  }
+
+  bind () {
+    this.el.addEventListener('mouseenter', () => {
+      this.y = this.el.offsetTop - window.pageYOffset
+      this.x = this.el.offsetLeft - window.pageXOffset
+
+      this.width = this.el.clientWidth
+      this.height = this.el.clientHeight
+    })
+
+    this.el.addEventListener('mousemove', e => {
+      const y = (e.clientY - this.y - this.height / 2) * this.options.y
+      const x = (e.clientX - this.x - this.width / 2) * this.options.x
+
+      this.move(x, y, this.options.s)
+    })
+
+    this.el.addEventListener('mouseleave', () => this.move(0, 0, this.options.rs))
+  }
+
+  move (x, y, speed) {
+    gsap.to(this.el, {
+      y,
+      x,
+      force3D: true,
+      overwrite: true,
+      duration: speed
+    })
+  }
+}
